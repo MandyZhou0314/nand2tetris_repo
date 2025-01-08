4e7697e54066de3d838b77bf4c5efeed1d48f014
Mandy Zhou <qiong_zhou999@163.com>
2025-01-08 18:36:38 +0800

    refactor

--- 08/vm_translator.py
+++ 08/vm_translator.py
@@ -2,19 +2,19 @@ import sys
 import os
 from parser import Parser
 
-def parse_vm_file(inputFileStr: str) -> list:
+def parse_vm_file(input_file_path: str) -> list:
     #if source is a folder, handle every vm file separately
-    if not ".vm" in inputFileStr:
+    if not ".vm" in input_file_path:
         print("not vm file!!!")
         return []
     #if source is a vm file
-    fileName = os.path.basename(inputFileStr).split(".vm")[0]
+    file_name = os.path.basename(input_file_path).split(".vm")[0]
     raw_commands = []
     asm_codes = []
-    with open(inputFileStr, "r") as file:
+    with open(input_file_path, "r") as file:
         for row in file:
             raw_commands.append(row)
-    p = Parser(raw_commands, fileName)
+    p = Parser(raw_commands, file_name)
     p.run_first_pass()
     p.run_second_pass()
     asm_codes = p.get_asm_codes()
@@ -24,27 +24,22 @@ def main():
     #open and save file
     if len(sys.argv) != 3:
         sys.exit("Wrong input")
-    inputPath = sys.argv[1]
-    outFileStr = sys.argv[2]
+    input_path = sys.argv[1]
+    out_file_path = sys.argv[2]
     asm_codes = []
-    if ".vm" not in inputPath:
-        
-        asm_codes += ["@256", "D=A", "@SP", "M=D"]
-        p = Parser(["call Sys.init 0"], "aa")
-        p.run_first_pass()
-        p.run_second_pass()
-        asm_codes += p.get_asm_codes()
-        
-        for filename in os.listdir(inputPath):
-            if filename.endswith('.vm'):
-                file_path = os.path.join(inputPath, filename)
+    if ".vm" not in input_path:
+        # add init code. 
+        # assume a dummy function frame (size=5) is pushed to stack before calling Sys.init
+        asm_codes = [f"@{256+5}", "D=A", "@SP", "M=D", "@Sys.init", "0;JMP"]
+        for file_name in os.listdir(input_path):
+            if file_name.endswith('.vm'):
+                file_path = os.path.join(input_path, file_name)
                 print(file_path)
                 asm_codes.extend(parse_vm_file(file_path))
-        
     else:
-        asm_codes = parse_vm_file(inputPath)
+        asm_codes = parse_vm_file(input_path)
     
-    with open(outFileStr, 'w', newline='\n') as file:
+    with open(out_file_path, 'w', newline='\n') as file:
         for line in asm_codes:
             file.write(f"{line}\n")
 
